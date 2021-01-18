76daa74bf94de5e1636f69da40e453ea4f4f552b
Eric Shen <ericshenyuhao@outlook.com>
2021-01-18 10:23:08 +0800

    update lvling

--- src/containers/guiyuan/guiyuanIndex.jsx
+++ src/containers/guiyuan/guiyuanIndex.jsx
@@ -15,19 +15,8 @@ class guiyuan extends Component {
 		return (	
 		<div>
 			<Row>
-				<Col span={20}>
-                    <Card title="南湖街道桂苑社区社会组织联合会" className="mg-top20">
-						<h2>组织简介：</h2>
-                        <p>嘉兴市南湖区南湖街道桂苑社区社会组织联合会成立于2017年9月。本团体的业务范围：为老年人群提供专业社工服务；承接政府相关部门、企事业单位、社会组织委托进行相关项目开发和服务；培育、孵化辖区内各类社会组织，给他们提供场地支持、团队建设、项目辅导、资源整合等服务；培养壮大社会工作人才队伍。</p>
-						<br></br>
-						<h2>组织活动：</h2>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E6%A1%82%E8%8B%91%E7%A4%BE%E5%8C%BA%E7%A4%BE%E4%BC%9A%E7%BB%84%E7%BB%87%E8%81%94%E5%90%88%E4%BC%9A-1.jpg" />
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E6%A1%82%E8%8B%91%E7%A4%BE%E5%8C%BA%E7%A4%BE%E4%BC%9A%E7%BB%84%E7%BB%87%E8%81%94%E5%90%88%E4%BC%9A-2.jpg" />
-						<br></br>
-						<br></br>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E6%A1%82%E8%8B%91%E7%A4%BE%E5%8C%BA%E7%A4%BE%E4%BC%9A%E7%BB%84%E7%BB%87%E8%81%94%E5%90%88%E4%BC%9A-3.jpg" />
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E6%A1%82%E8%8B%91%E7%A4%BE%E5%8C%BA%E7%A4%BE%E4%BC%9A%E7%BB%84%E7%BB%87%E8%81%94%E5%90%88%E4%BC%9A-4.jpg" />
-                    </Card> 
+				<Col span={20} className="mg-top20">
+					<img height="5258" width="1622" src="http://pontt9qvy.bkt.clouddn.com/%E5%98%89%E5%85%B4%E5%B8%82%E5%8D%97%E6%B9%96%E5%8C%BA%E5%8D%97%E6%B9%96%E8%A1%97%E9%81%93%E6%A1%82%E8%8B%91%E7%A4%BE%E5%8C%BA%E7%A4%BE%E4%BC%9A%E7%BB%84%E7%BB%87%E8%81%94%E5%90%88%E4%BC%9A.jpg"></img>
                 </Col>
             </Row>
 		</div>
--- src/containers/lvlingyanyuren/lvlingyanyurenIndex.jsx
+++ src/containers/lvlingyanyuren/lvlingyanyurenIndex.jsx
@@ -15,19 +15,8 @@ class lvlingyanyuren extends Component {
 		return (	
 		<div>
 			<Row>
-				<Col span={20}>
-                    <Card title="绿菱烟雨人社会服务工作室" className="mg-top20">
-						<h2>组织简介：</h2>
-                        <p>提供服务、反映诉求、规范行为，改善社会组织发展环境，提升社会组织建设质量。配合业务主管部门建立与我社区发展水平相适应、布局合理、结构优化、功能到位、作用明显的社会组织发展体系和法制健全、行为规范、分类管理、分级负责的社会组织管理体系。运用专业化方法开展社会工作实务，努力实现新形势下化解社会矛盾，解决社会问题，维护社会稳定，促进社会和谐的工作目标。</p>
-						<br></br>
-						<h2>组织活动：</h2>
-						<p>手工爱好都制作烟雨特色的菱形香囊作品</p>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E8%8F%B1%E5%BD%A2%E9%A6%99%E5%9B%8A.png"/>
-						<br></br>
-						<br></br>
-						<p>工作室风采展示</p>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E5%B7%A5%E4%BD%9C%E5%AE%A4%E9%A3%8E%E9%87%87%E5%B1%95%E7%A4%BA.png" />
-                    </Card> 
+				<Col span={20} className="mg-top20">
+					<img height="4189" width="1622" src="http://pontt9qvy.bkt.clouddn.com/%E5%98%89%E5%85%B4%E5%B8%82%E5%8D%97%E6%B9%96%E5%8C%BA%E5%8D%97%E6%B9%96%E8%A1%97%E9%81%93%E7%BB%BF%E8%8F%B1%E7%83%9F%E9%9B%A8%E4%BA%BA%E7%A4%BE%E4%BC%9A%E6%9C%8D%E5%8A%A1%E5%B7%A5%E4%BD%9C%E5%AE%A4.jpg"></img>
                 </Col>
             </Row>
 		</div>
