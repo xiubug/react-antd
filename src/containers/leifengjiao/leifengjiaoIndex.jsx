4521b3043e793b839a70e4b675333509a4d9f8ab
Eric Shen <ericshenyuhao@outlook.com>
2021-01-18 10:07:35 +0800

    update leifengjiao

--- src/containers/leifengjiao/leifengjiaoIndex.jsx
+++ src/containers/leifengjiao/leifengjiaoIndex.jsx
@@ -15,22 +15,8 @@ class leifengjiao extends Component {
 		return (	
 		<div>
 			<Row>
-				<Col span={20}>
-                    <Card title="民北社区雷锋角" className="mg-top20">
-						<img height="50" width="50" src="http://pontt9qvy.bkt.clouddn.com/%E6%B0%91%E5%8C%97%E7%A4%BE%E5%8C%BA%E9%9B%B7%E9%94%8B%E8%A7%92%E7%A4%BE%E4%BC%9A%E5%B7%A5%E4%BD%9C%E5%AE%A4.jpg" />
-						<h2>组织简介：</h2>
-                        <p>2017年9月登记注册，服务内容：敬老慰问活动；助残慰问活动；帮扶活动；新居民家庭教育；社工项目服务。</p>
-						<br></br>
-						<h2>组织活动：</h2>
-						<p>农民工子女服务项目，和《“花young年华”新居民子女周末欢乐营》</p>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E5%86%9C%E6%B0%91%E5%B7%A5%E5%AD%90%E5%A5%B3%E6%9C%8D%E5%8A%A1%E9%A1%B9%E7%9B%AE.jpg" />
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E5%86%9C%E6%B0%91%E5%B7%A5%E5%AD%90%E5%A5%B3%E6%9C%8D%E5%8A%A1%E9%A1%B9%E7%9B%AE2.jpg" />
-						<br></br>
-						<br></br>
-						<p>入户走访，了解需求。针对辖区一位青少年开展帮扶帮教。</p>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E5%85%A5%E6%88%B7%E8%B5%B0%E8%AE%BF.jpg" />
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E5%85%A5%E6%88%B7%E8%B5%B0%E8%AE%BF2.jpg" />
-                    </Card> 
+				<Col span={20} className="mg-top20">
+					<img height="5832" width="1621" src="http://pontt9qvy.bkt.clouddn.com/1%E9%9B%B7%E9%94%8B%E8%A7%92%E7%A4%BE%E5%8C%BA%E7%A4%BE%E4%BC%9A%E7%BB%84%E7%BB%87%E6%9C%8D%E5%8A%A1%E4%B8%AD%E5%BF%83.jpg"></img>
                 </Col>
             </Row>
 		</div>
--- src/containers/lexinshehuizuzhi/lexinshehuizuzhiIndex.jsx
+++ src/containers/lexinshehuizuzhi/lexinshehuizuzhiIndex.jsx
@@ -15,19 +15,8 @@ class lexinshehuizuzhi extends Component {
 		return (	
 		<div>
 			<Row>
-				<Col span={20}>
-                    <Card title="万洲社区乐心社会组织服务中心" className="mg-top20">
-						<h2>组织简介：</h2>
-                        <p>完善残疾人社会组织内部治理结构的需要，有效树立残疾人社会组织形象地位的窗口，更规范地承接政府转移职能和购买服务，贯彻落实残疾人社会组织党建工作的要求。为更多的残疾人开展社会服务工作。让残疾朋友们建立起自信，融入社会。</p>
-						<br></br>
-						<h2>组织活动：</h2>
-						<p>情系孤老 童心送暖</p>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E6%83%85%E7%B3%BB%E5%AD%A4%E8%80%81.png"/>
-						<br></br>
-						<br></br>
-						<p>为残障家庭拍摄全家福</p>
-						<img height="300" width="400" src="http://pontt9qvy.bkt.clouddn.com/%E5%85%A8%E5%AE%B6%E7%A6%8F.jpg" />
-                    </Card> 
+				<Col span={20} className="mg-top20">
+					<img height="5409" width="1621" src="http://pontt9qvy.bkt.clouddn.com/1%E4%B9%90%E5%BF%83%E7%A4%BE%E4%BC%9A%E7%BB%84%E7%BB%87%E7%AE%80%E4%BB%8B.jpg"></img>
                 </Col>
             </Row>
 		</div>
